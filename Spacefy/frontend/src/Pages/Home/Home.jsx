13894b7d392e9e4b9bc2a946472aaba2c3fa7b24
Lucas Picanco <lucasferreira3254@outlook.com>
2025-04-18 23:18:11 -0300

    feat: install Tailwind CSS, create Header component and start Landing Page layout

--- Spacefy/frontend/src/Pages/Home/Home.jsx
+++ Spacefy/frontend/src/Pages/Home/Home.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Container, Title, StyledButton } from './StyledHome'
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Title, StyledButton } from "./StyledHome";
+import Header from "../../Components/Header/Header";
 const Home = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <Container>
+    <>
+      <Header />
       <Title>Você está na Home!</Title>
-      <StyledButton onClick={() => navigate('/')}>
+      <StyledButton onClick={() => navigate("/")}>
         Voltar para Landing Page
       </StyledButton>
-    </Container>
-  )
-}
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- Spacefy/frontend/src/Pages/LaddingPage/LaddingPage.jsx
+++ Spacefy/frontend/src/Pages/LaddingPage/LaddingPage.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Container, Title, StyledButton } from './StyledLaddingPage'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Title, StyledButton } from "./StyledLaddingPage";
+import Header from "../../Components/Header/Header";
+import banner from "../../assets/Banner.svg";
 
 const Landing = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <Container>
+    <>
+      <Header />
+      <section>
+        <div className="relative w-full h-[50vh]">
+          <img
+            src={banner}
+            alt="Banner"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 flex flex-col justify-center items-start text-white p-35">
+            <h2 className="text-4xl font-bold">
+              Seu espaço ideal, perto de você.
+            </h2>
+            <p className="text-4xl font-bold">
+              Encontre, reserve e aproveite com facilidade!
+            </p>
+          </div>
+        </div>
+      </section>
       <Title>Bem-vindo à Landing Page</Title>
-      <StyledButton onClick={() => navigate('/home')}>
+      <StyledButton onClick={() => navigate("/home")}>
         Ir para Home
       </StyledButton>
-    </Container>
-  )
-}
+    </>
+  );
+};
 
-export default Landing
+export default Landing;

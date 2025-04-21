11675733df74d017c2398b384fc45cbc35cb9cef
Arthur Silvino da Silva <arthursilvino902@gmail.com>
2025-04-21 11:56:21 -0300

    registra rotas de histórico no servidor principal

--- Spacefy/backend/src/server.ts
+++ Spacefy/backend/src/server.ts
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRoutes";
 import spaceRouter from "./routes/spaceRoutes";
 import authRoutes from "./routes/authRoutes";
 import paymentRoutes from "./routes/paymentRoutes";
+import viewHistoryRouter from "./routes/viewhistoryRoutes";
 import cors from "cors";
 
 
@@ -17,7 +18,8 @@ app.use(express.json()); // Middleware para JSON
 app.use("/users", userRouter); // Rotas de usuários
 app.use("/spaces", spaceRouter); // Rotas de espaços
 app.use("/auth", authRoutes); // Rotas de autenticação
-app.use("/payments", paymentRoutes);// Rotas de Pagamento Seguro 
+app.use("/payments", paymentRoutes);// Rotas de Pagamento Seguro
+app.use("/view-history", viewHistoryRouter);
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;

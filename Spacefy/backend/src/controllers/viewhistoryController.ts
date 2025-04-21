afe67c2fe6386423da760982914741c2542d5a0e
Arthur Silvino da Silva <arthursilvino902@gmail.com>
2025-04-21 12:03:45 -0300

    implementando a função registerViewHistory

---
+++ Spacefy/backend/src/controllers/viewhistoryController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import viewhistoryModel from "../models/viewhistoryModel";
+
+// Regitro de Vizualização
+export const registerViewHistory = async (req: Request, res: Response) => {
+  try {
+    const { user_id, space_id } = req.body;
+
+    if (!user_id || !space_id) {
+      return res.status(400).json({ error: "user_id e space_id são obrigatórios." });
+    }
+
+    const newViewHistory = new viewhistoryModel({ user_id, space_id });
+    await newViewHistory.save();
+
+    res.status(201).json(newViewHistory);
+  } catch (error) {
+    console.error("Erro ao registrar visualização:", error);
+    res.status(500).json({ error: "Erro ao registrar visualização" });
+  }
+};
+
+//Busca Historico de Vizualização
+export const getViewHistoryByUser = async (req: Request, res: Response) => {
+    try {
+      const { user_id } = req.params;
+  
+      if (!user_id) {
+        return res.status(400).json({ error: "user_id é obrigatório." });
+      }
+  
+      const history = await viewhistoryModel
+        .find({ user_id })
+        .populate("space_id"); // opcional: mostra detalhes do espaço
+  
+      res.status(200).json(history);
+    } catch (error) {
+      console.error("Erro ao buscar histórico:", error);
+      res.status(500).json({ error: "Erro ao buscar histórico de visualizações." });
+    }
+  };

0c1f70479588fc463a1947514dd5dbfe22bdf346
Victor Vieira Demarque <victordeemarque7@gmail.com>
2025-04-28 13:15:38 -0300

    Update authRoutes.ts
    Criando Rotas de Recuperaçao de Senha

--- Spacefy/backend/src/routes/authRoutes.ts
+++ Spacefy/backend/src/routes/authRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { login } from "../controllers/authController"; // certifique-se que o caminho está certo
+import { login, requestPasswordReset, resetPassword } from "../controllers/authController"; // certifique-se que o caminho está certo
 
 const Loginrouter = express.Router();
 
 // Rota de login
 Loginrouter.post("/login", login as express.RequestHandler);
+
+// Rota para solicitar recuperação de senha
+Loginrouter.post("/request-password-reset", requestPasswordReset as express.RequestHandler);
+
+// Rota para redefinir nova senha
+Loginrouter.post("/reset-password", resetPassword as express.RequestHandler);
 
 export default Loginrouter;

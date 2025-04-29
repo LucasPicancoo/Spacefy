52509253b1e1895aaf6105db9b852760e2c9cdac
Lucas Picanco <lucasferreira3254@outlook.com>
2025-04-29 16:19:08 -0300

    Correção nas rotas

--- Spacefy/frontend/src/Routes/Router.jsx
+++ Spacefy/frontend/src/Routes/Router.jsx
@@ -6,13 +6,9 @@ import Login from '../Pages/Login/Login'
 import Perfil from '../Pages/Perfil'
 import Descobrir from '../Pages/Descobrir/Descobrir'
 import Espaço from '../Pages/Espaço/Espaço'
-<<<<<<< HEAD
-import Perfil_Locatario from '../Pages/Perfil_Locatario/Perfil_Locatario'
-=======
 import DashboardLocatario from '../Pages/Locatario/DashboardLocatario'
 import CadastrarEspaco from '../Pages/CadastrarEspaco.jsx/CadastrarEspaco'
 
->>>>>>> 56d46bd812a1f22a931fa60575122931ff2e79e2
 const AppRoutes = () => {
   return (
     <Router>
@@ -24,12 +20,8 @@ const AppRoutes = () => {
         <Route path="/Perfil" element={<Perfil />} />
         <Route path="/Descobrir" element={<Descobrir />} />
         <Route path="/Espaço" element={<Espaço />} />
-<<<<<<< HEAD
-        <Route path="/Perfil_Locatario" element={<Perfil_Locatario />} />
-=======
         <Route path="/DashboardLocatario" element={<DashboardLocatario />} />
         <Route path="/CadastrarEspaco" element={<CadastrarEspaco />} />
->>>>>>> 56d46bd812a1f22a931fa60575122931ff2e79e2
       </Routes>
     </Router>
   )

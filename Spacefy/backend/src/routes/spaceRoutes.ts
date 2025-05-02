0e0b2285d07f9c39b47290f82d8c28f69e0a02fb
Yuque7 <yuque7@gmail.com>
2025-05-01 22:48:42 -0300

    corrigindo erros da rota space

--- Spacefy/backend/src/routes/spaceRoutes.ts
+++ Spacefy/backend/src/routes/spaceRoutes.ts
@@ -6,22 +6,23 @@ import {
   updateSpace,
   deleteSpace,
 } from "../controllers/spaceController";
+import { validateAndGetTokenData } from "../middlewares/token";
 
 const spaceRouter = express.Router();
 
 // Rota para listar todos os espaços
-spaceRouter.get("/getAllSpaces", getAllSpaces);
+spaceRouter.get("/getAllSpaces", validateAndGetTokenData, getAllSpaces);
 
 // Rota para obter um espaço por ID
-spaceRouter.get("/getSpaceById/:id", getSpaceById);
+spaceRouter.get("/getSpaceById/:id", validateAndGetTokenData, getSpaceById);
 
 // Rota para criar um novo espaço
-spaceRouter.post("/createSpace", createSpace);
+spaceRouter.post("/createSpace", validateAndGetTokenData, createSpace);
 
 // Rota para atualizar um espaço por ID
-spaceRouter.put("/updateSpace", updateSpace);
+spaceRouter.put("/updateSpace", validateAndGetTokenData, updateSpace);
 
 // Rota para excluir um espaço por ID
-spaceRouter.delete("/deleteSpace", deleteSpace);
+spaceRouter.delete("/deleteSpace", validateAndGetTokenData, deleteSpace);
 
 export default spaceRouter;

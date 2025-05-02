1f582dc692af776ae4ba6b29f2ca9af1dc051fcf
Yuque7 <yuque7@gmail.com>
2025-05-01 23:14:58 -0300

    ...

--- Spacefy/backend/src/routes/spaceRoutes.ts
+++ Spacefy/backend/src/routes/spaceRoutes.ts
@@ -11,10 +11,10 @@ import { validateAndGetTokenData } from "../middlewares/token";
 const spaceRouter = express.Router();
 
 // Rota para listar todos os espaços
-spaceRouter.get("/getAllSpaces", validateAndGetTokenData, getAllSpaces);
+spaceRouter.get("/getAllSpaces", getAllSpaces);
 
 // Rota para obter um espaço por ID
-spaceRouter.get("/getSpaceById/:id", validateAndGetTokenData, getSpaceById);
+spaceRouter.get("/getSpaceById/:id", getSpaceById);
 
 // Rota para criar um novo espaço
 spaceRouter.post("/createSpace", validateAndGetTokenData, createSpace);

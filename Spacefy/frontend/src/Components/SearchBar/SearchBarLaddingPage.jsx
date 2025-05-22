73852a4d0fea2ad6011509cc5b8d46ad0808dd3e
Lucas Picanco <lucasferreira3254@outlook.com>
2025-05-22 15:20:01 -0300

    Fix temporario no botao de pesquisar da barra de pesquisa da landing page

--- Spacefy/frontend/src/Components/SearchBar/SearchBarLaddingPage.jsx
+++ Spacefy/frontend/src/Components/SearchBar/SearchBarLaddingPage.jsx
@@ -74,7 +74,7 @@ export default function SearchBarLaddingPage() {
 
       {/* Botão de Pesquisa */}
       <button
-        type="button" onClick={() => navigate("/Espaço")}
+        type="button" onClick={() => navigate("/Descobrir")}
         className="bg-[#1EACE3] hover:bg-[#32C6FF] hover:scale-[1.05] text-white font-medium px-8 py-2.5 rounded-lg transition-all duration-300 cursor-pointer" style={{ boxShadow: '0 7px 10px rgba(0, 0, 0, 0.30)' }}>
         Pesquisar
       </button>

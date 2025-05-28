0ac6cf92a42683ba9c0bf7d9d9afe353ba67cb8b
VitinDemarque <victordeemarque7@gmail.com>
2025-05-28 19:34:33 -0300

    Atualizando BlockedDateRoutes

--- Spacefy/backend/src/routes/blockedDateRoutes.ts
+++ Spacefy/backend/src/routes/blockedDateRoutes.ts
@@ -1,11 +1,16 @@
+// routes/blockedDateRoutes.ts
 import express from "express";
-import { blockDate, getBlockedDatesBySpace, unblockDate } from "../controllers/blockedDateController";
+import {
+  blockDate,
+  getBlockedDatesBySpace,
+  unblockDate,
+} from "../controllers/blockedDateController";
 import { validateAndGetTokenData } from "../middlewares/token";
 
 const router = express.Router();
 
-router.post("/", validateAndGetTokenData, blockDate); // Bloquear nova data
-router.get("/:spaceId", getBlockedDatesBySpace); // Listar datas bloqueadas de um espaço
-router.delete("/", validateAndGetTokenData, unblockDate); // Desbloquear data
+router.post("/block", validateAndGetTokenData, blockDate);
+router.get("/:spaceId", getBlockedDatesBySpace);
+router.delete("/unblock", validateAndGetTokenData, unblockDate);
 
 export default router;

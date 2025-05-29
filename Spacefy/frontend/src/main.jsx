6103ffead91224c7204c7c1cf3537582993f7d44
Lucas Picanco <lucasferreira3254@outlook.com>
2025-05-29 01:53:45 -0300

    Fix no main.jsx para o deploy

--- Spacefy/frontend/src/main.jsx
+++ Spacefy/frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { UserProvider } from "./Contexts/userContext.jsx";
+import { UserProvider } from "./Contexts/UserContext.jsx";
 import './index.css'
 import App from './App.jsx'
 

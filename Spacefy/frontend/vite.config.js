9fb89f259193cd6d56c5eee0b9498059056687c8
Lucas Picanco <lucasferreira3254@outlook.com>
2025-05-29 01:19:22 -0300

    Mudanças no vite.config.js para o deploy

--- Spacefy/frontend/vite.config.js
+++ Spacefy/frontend/vite.config.js
@@ -4,5 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })

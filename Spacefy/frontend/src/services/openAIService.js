370476dc131968b0a641303a8e1aa37125417f15
Lucas Picanco <lucasferreira3254@outlook.com>
2025-06-11 19:53:31 -0300

    Fix na rota do assistente virtual

--- Spacefy/frontend/src/services/openAIService.js
+++ Spacefy/frontend/src/services/openAIService.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const openAIService = {
     async processUserPrompt(prompt) {
-        const response = await api.post("/chat/processar-pesquisa", { prompt });
+        const response = await api.post("/openai/processar-pesquisa", { prompt });
         return response.data;
     }
 }

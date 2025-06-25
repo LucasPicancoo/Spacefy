25666e7cccd7d9f60b4ed2c6d5f895484458443f
Yuri <yuque7@gmail.com>
2025-06-25 14:37:56 -0300

    Config no Redis DB

--- Spacefy/backend/src/config/redisConfig.ts
+++ Spacefy/backend/src/config/redisConfig.ts
@@ -5,9 +5,9 @@ dotenv.config();
 
 const redisUrl = process.env.REDIS_URL;
 
-if (!redisUrl) {
-  throw new Error('REDIS_URL não definida no .env');
-}
+//if (!redisUrl) {
+//  throw new Error('REDIS_URL não definida no .env');
+//}
 
 const redisClient = new Redis(redisUrl!);
 
